11f95bcf2012cdad4733136630f58f6f7e455545
musayko <musa.ozdemir@hotmail.comclear>
2025-01-01 18:12:38 +0200

    Update: Configure Next.js for static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     domains: [], // Add any external image domains if needed
   },
